623d99de34eae2710ee43cd7f54eb31b34b154ae
grassog <grassog@NBK04975.int.master.lan>
2023-04-20 12:03:29 +0200

    creato primo model e pacchetto model

---
+++ Azienda/src/main/java/com/giovanni/Azienda/model/Impiegati.java
@@ -0,0 +1,18 @@
+package com.giovanni.Azienda.model;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Impiegati implements Serializable{
+	private static final long serialVersionUID = -1821041931068958512L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	
+	
+}
