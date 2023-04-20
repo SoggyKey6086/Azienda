065dab7c9341710a9dbdf5920844681fb5abb7bc
biaforaa <biaforaa@NBK04974.int.master.lan>
2023-04-20 13:01:02 +0200

    rep , dip

---
+++ Azienda/src/main/java/com/giovanni/Azienda/model/Dipartimenti.java
@@ -0,0 +1,54 @@
+package com.giovanni.Azienda.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table
+public class Dipartimenti implements Serializable {
+	private static final long serialVersionUID = -5677868630895730332L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long IdDip;
+    
+    @Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String descrizione;
+	
+
+	public Long getIdDip() {
+		return IdDip;
+	}
+
+	public void setIdDip(Long idDip) {
+		IdDip = idDip;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	
+	
+}
---
+++ Azienda/src/main/java/com/giovanni/Azienda/model/Reparti.java
@@ -0,0 +1,66 @@
+package com.giovanni.Azienda.model;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Reparti implements Serializable{
+	private static final long serialVersionUID = 6730806279574944112L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idRep;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String descrizione;
+	
+	@OneToMany(mappedBy = "reparto",cascade = CascadeType.ALL)
+	private Set<DatiAziendali> da = new HashSet<DatiAziendali>();
+	
+	
+	public long getIdRep() {
+		return idRep;
+	}
+
+	public void setIdRep(long idRep) {
+		this.idRep = idRep;
+	}
+
+	public Set<DatiAziendali> getDa() {
+		return da;
+	}
+
+	public void setDa(Set<DatiAziendali> da) {
+		this.da = da;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	
+}
