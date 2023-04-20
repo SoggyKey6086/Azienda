bf96803e7e89bfed55e3e487472d161924fc3370
divinoe <divinoe@NBK04992.int.master.lan>
2023-04-20 12:59:21 +0200

    create

---
+++ Azienda/src/main/java/com/giovanni/Azienda/model/DatiAziendali.java
@@ -0,0 +1,89 @@
+package com.giovanni.Azienda.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class DatiAziendali implements Serializable{
+	private static final long serialVersionUID = 6054072049247454700L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idDA;
+
+	@Column(nullable = false)
+	private String email;
+	
+	@Column(nullable = false)
+	private Date data_nascita;
+	
+	@Column(nullable = false)
+	private Date data_assunzione;
+	
+	@Column(nullable = false)
+	private float salario;
+	
+	@ManyToOne
+	@JoinColumn(name="id_rep", nullable=false)
+	private Reparto reparto;
+
+	public long getIdDA() {
+		return idDA;
+	}
+
+	public void setIdDA(long idDA) {
+		this.idDA = idDA;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getData_nascita() {
+		return data_nascita;
+	}
+
+	public void setData_nascita(Date data_nascita) {
+		this.data_nascita = data_nascita;
+	}
+
+	public Date getData_assunzione() {
+		return data_assunzione;
+	}
+
+	public void setData_assunzione(Date data_assunzione) {
+		this.data_assunzione = data_assunzione;
+	}
+
+	public float getSalario() {
+		return salario;
+	}
+
+	public void setSalario(float salario) {
+		this.salario = salario;
+	}
+
+	public Reparto getReparto() {
+		return reparto;
+	}
+
+	public void setReparto(Reparto reparto) {
+		this.reparto = reparto;
+	}
+	
+	
+}
