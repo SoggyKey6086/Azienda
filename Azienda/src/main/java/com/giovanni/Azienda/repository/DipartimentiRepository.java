d0e9e7813c0b1d2b3ed6e74b504b25eae723dcdb
divinoe <divinoe@NBK04992.int.master.lan>
2023-04-21 17:14:54 +0200

    lhwevb

--- Azienda/src/main/java/com/giovanni/Azienda/repository/DipartimentiRepository.java
+++ Azienda/src/main/java/com/giovanni/Azienda/repository/DipartimentiRepository.java
@@ -12,12 +12,12 @@ import com.giovanni.Azienda.model.Dipartimenti;
 @Repository("dipartimentiRepository")
 public interface DipartimentiRepository extends JpaRepository<Dipartimenti, Integer>{
 	
-	@Query(value = "Select * from reparti where  = ?1", nativeQuery = true)
+	@Query(value = "Select * from dipartimenti where  = ?1", nativeQuery = true)
 	public Optional<Dipartimenti> findById(Integer idRep);
 	
-	@Query(value = "SELECT reparti.nome AS reparti, COUNT(*) AS totale_impiegati FROM reparti INNER JOIN impiegati  ON "
-			+ "reparti.id_rep = impiegati.id_rep GROUP BY reparti.nome", nativeQuery = true)
-	public List<String[]> findQtaImpXRep(int idRep);
+	@Query(value = "SELECT dipartimenti.nome AS dipartimenti, COUNT(*) AS totale_impiegati FROM dipartimenti INNER JOIN impiegati  ON "
+			+ "dipartimenti.id_dip = impiegati.id_dip GROUP BY dipartimenti.nome", nativeQuery = true)
+	public List<String[]> findQtaImpXImp(int idDip);
 	
 
 }
