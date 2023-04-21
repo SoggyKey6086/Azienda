17ed1cfe4bee78a2376cd4946f53141b8d5fa1b5
grassog <grassog@NBK04975.int.master.lan>
2023-04-21 14:40:27 +0200

    commit

--- Azienda/src/main/java/com/giovanni/Azienda/repository/ImpiegatiRepository.java
+++ Azienda/src/main/java/com/giovanni/Azienda/repository/ImpiegatiRepository.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.giovanni.Azienda.model.Impiegati;
 
+@Repository("impiegatiRepository")
 public interface ImpiegatiRepository extends JpaRepository<Impiegati, Integer>{
 	@Query(value = "Select * from reparti where  = ?1", nativeQuery = true)
 	public Optional<Impiegati> findById(Integer idImp);
--- Azienda/src/main/java/com/giovanni/Azienda/repository/SalariRepository.java
+++ Azienda/src/main/java/com/giovanni/Azienda/repository/SalariRepository.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.giovanni.Azienda.model.Salario;
 
+@Repository("salariRepository")
 public interface SalariRepository extends JpaRepository<Salario, Integer> {
 	@Query(value = "Select * from reparti where  = ?1", nativeQuery = true)
 	public Optional<Salario> findById(Integer idS);
