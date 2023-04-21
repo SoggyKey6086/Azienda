7e8024acf0d4ae8b0a56810a3c14012a07c378dd
divinoe <divinoe@NBK04992.int.master.lan>
2023-04-21 17:33:52 +0200

    adbfet

--- Azienda/src/main/java/com/giovanni/Azienda/repository/DipartimentiRepository.java
+++ Azienda/src/main/java/com/giovanni/Azienda/repository/DipartimentiRepository.java
@@ -13,7 +13,7 @@ import com.giovanni.Azienda.model.Dipartimenti;
 public interface DipartimentiRepository extends JpaRepository<Dipartimenti, Integer>{
 	
 	@Query(value = "Select * from dipartimenti where  = ?1", nativeQuery = true)
-	public Optional<Dipartimenti> findById(Integer idRep);
+	public Optional<Dipartimenti> findById(Integer idDip);
 	
 	@Query(value = "SELECT dipartimenti.nome AS dipartimenti, COUNT(*) AS totale_impiegati FROM dipartimenti INNER JOIN impiegati  ON "
 			+ "dipartimenti.id_dip = impiegati.id_dip GROUP BY dipartimenti.nome", nativeQuery = true)
