b21bf71c1a234f2ce070b3827b28c228696ed7b6
grassog <grassog@NBK04975.int.master.lan>
2023-04-21 15:21:14 +0200

    Service

--- Azienda/src/main/java/com/giovanni/Azienda/repository/RepartiRepository.java
+++ Azienda/src/main/java/com/giovanni/Azienda/repository/RepartiRepository.java
@@ -14,6 +14,7 @@ public interface RepartiRepository extends JpaRepository<Reparti, Integer>{
 	@Query(value = "Select * from reparti where  = ?1", nativeQuery = true)
 	public Optional<Reparti> findById(Integer idRep);
 	
-	@Query(value = "SELECT reparti.nome AS reparti, COUNT(*) AS totale_impiegati FROM reparti INNER JOIN impiegati  ON reparti.id_rep = impiegati.id_rep GROUP BY reparti.nome", nativeQuery = true)
+	@Query(value = "SELECT reparti.nome AS reparti, COUNT(*) AS totale_impiegati FROM reparti INNER JOIN impiegati  ON "
+			+ "reparti.id_rep = impiegati.id_rep GROUP BY reparti.nome", nativeQuery = true)
 	public List<String[]> findQtaImpXRep(int idRep);
 }
