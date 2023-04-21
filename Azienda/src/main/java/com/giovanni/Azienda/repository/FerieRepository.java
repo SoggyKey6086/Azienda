ff945f0d0df81d2e0841a6632e36b78d41230a1c
divinoe <divinoe@NBK04992.int.master.lan>
2023-04-21 11:39:21 +0200

    create

---
+++ Azienda/src/main/java/com/giovanni/Azienda/repository/FerieRepository.java
@@ -0,0 +1,20 @@
+package com.giovanni.Azienda.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.giovanni.Azienda.model.Ferie;
+@Repository
+public interface FerieRepository extends JpaRepository<Ferie, String>{
+
+	@Query(value = "SELECT impiegati.nome, impiegati.cognome, COUNT(*) AS totale_ferie " +
+            "FROM ferie " +
+            "INNER JOIN impiegati ON impiegati.id_imp = ferie.id_imp " +
+            "GROUP BY impiegati.id_imp", 
+    nativeQuery = true)
+	public List<String[]> findTotImpFerie();
+	
+}
