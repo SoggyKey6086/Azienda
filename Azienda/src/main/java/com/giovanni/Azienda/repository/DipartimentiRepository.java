9c5fc05153b07a017c0a1026275d3a3f3ea65b95
divinoe <divinoe@NBK04992.int.master.lan>
2023-04-21 17:09:27 +0200

    update

---
+++ Azienda/src/main/java/com/giovanni/Azienda/repository/DipartimentiRepository.java
@@ -0,0 +1,23 @@
+package com.giovanni.Azienda.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.giovanni.Azienda.model.Dipartimenti;
+
+@Repository("dipartimentiRepository")
+public interface DipartimentiRepository extends JpaRepository<Dipartimenti, Integer>{
+	
+	@Query(value = "Select * from reparti where  = ?1", nativeQuery = true)
+	public Optional<Dipartimenti> findById(Integer idRep);
+	
+	@Query(value = "SELECT reparti.nome AS reparti, COUNT(*) AS totale_impiegati FROM reparti INNER JOIN impiegati  ON "
+			+ "reparti.id_rep = impiegati.id_rep GROUP BY reparti.nome", nativeQuery = true)
+	public List<String[]> findQtaImpXRep(int idRep);
+	
+
+}
