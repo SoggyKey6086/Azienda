846d200674df5d964a25cf6e8c6fb65b8c2b2357
divinoe <divinoe@NBK04992.int.master.lan>
2023-04-21 15:29:20 +0200

    update

--- Azienda/src/main/java/com/giovanni/Azienda/repository/PresenzeRepository.java
+++ Azienda/src/main/java/com/giovanni/Azienda/repository/PresenzeRepository.java
@@ -21,4 +21,11 @@ public interface PresenzeRepository extends JpaRepository<Presenze, Integer> {
 			+ "FROM Impiegati JOIN dati_aziendali ON impiegati.id_imp = dati_aziendali.id_rep LEFT JOIN presenze ON "
 			+ "impiegati.id_imp = presenze.id_imp GROUP BY impiegati.id_imp", nativeQuery = true)
 	public List<String[]> findQtaAssenze(int idPre);
+	
+	@Query(value = "SELECT Impiegati.id_imp, Impiegati.nome, Impiegati.cognome, SUM(TIME_TO_SEC(TIMEDIFF(Presenze.orario_fine, Presenze.orario_inizio))/3600) AS Numero_Ore_Lavorate\r\n"
+			+ "FROM Presenze\r\n"
+			+ "JOIN Impiegati ON Presenze.id_imp = Impiegati.id_imp\r\n"
+			+ "WHERE Presenze.data BETWEEN ?1 AND  ?2\r\n"
+			+ "GROUP BY Impiegati.id_imp, Impiegati.nome, Impiegati.cognome", nativeQuery= true)
+	public List<String[]> findByIdImp(int idPre);
 }
