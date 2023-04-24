dcf635d7add8dbb79ab0738d7e48b02fe1e6d49e
divinoe <divinoe@NBK04992.int.master.lan>
2023-04-24 15:26:32 +0200

    updating

--- Azienda/src/main/java/com/giovanni/Azienda/service/DipartimentiService.java
+++ Azienda/src/main/java/com/giovanni/Azienda/service/DipartimentiService.java
@@ -3,15 +3,17 @@ package com.giovanni.Azienda.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
 import com.giovanni.Azienda.model.Dipartimenti;
 
-
+@Service
 public interface DipartimentiService {
 
 	void saveDipartimenti(Dipartimenti Dipartimenti);
 	List<Dipartimenti> getAll();
 	Optional<Dipartimenti> findById(int id);
 	void deleteDipartimenti(Dipartimenti Dipartimenti);
-	List<String[]> findQtaImpXImp(int idDip);
+	List<String[]> findQtaImpXDip(int idDip);
 	
 }
